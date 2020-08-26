1c41ac7a2db5c3bd6fb8004eecb4fdbc9898e2b0
tedisy47123 <tedisya47@gmail.com>
2020-08-27 03:24:51 +0700

    uppdate_urls

--- kmean/views.py
+++ kmean/views.py
@@ -12,13 +12,14 @@ def index(request):
 	return render(request,'login.html')
 def login(request):
 	if request.method == 'POST':
-		# print(requests)
-		user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+		print(request.POST)
+		user = authenticate(username=request.POST['username'], password=request.POST['password'])
+		print(user)
 		if user is not None:
 			if user.is_active is True:
 				auth_login(request, user)
 				messages.add_message(request, messages.INFO, 'Selamat datang.')
-				return redirect('/home/')
+				return redirect('/fuzzy/')
 				# return HttpResponse('ok')
 			else:
 				return HttpResponse('gagal')
