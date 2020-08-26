b041e593fa43ee552bd099d3355d224db408e5c7
tedisy47123 <tedisya47@gmail.com>
2020-08-27 03:08:38 +0700

    uppdate_urls

--- kmean/views.py
+++ kmean/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.shortcuts import  render, redirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib import messages
+
+# import requests
+import json
 
 # Create your views here.
 def index(request):
 	return render(request,'login.html')
 def login(request):
-	if requests.method == 'POST':
+	if request.method == 'POST':
 		# print(requests)
-		user = authenticate(username=requests.POST['username'], password=requests.POST['password'])
+		user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
 		if user is not None:
 			if user.is_active is True:
-				auth_login(requests, user)
-				messages.add_message(requests, messages.INFO, 'Selamat datang.')
+				auth_login(request, user)
+				messages.add_message(request, messages.INFO, 'Selamat datang.')
 				return redirect('/home/')
 				# return HttpResponse('ok')
+			else:
+				return HttpResponse('gagal')
+		else:
+			return HttpResponse('gagal')
 	else:
 		return redirect('/fuzzy/')
 def register(request):
