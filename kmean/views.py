5e822674edd183885aa2c0a2c4444929a07468eb
tedisy47123 <tedisya47@gmail.com>
2020-08-27 02:54:25 +0700

    uppdate_auth

--- kmean/views.py
+++ kmean/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'login.html')
+def login(request):
+	if requests.method == 'POST':
+		# print(requests)
+		user = authenticate(username=requests.POST['username'], password=requests.POST['password'])
+		if user is not None:
+			if user.is_active is True:
+				auth_login(requests, user)
+				messages.add_message(requests, messages.INFO, 'Selamat datang.')
+				return redirect('/home/')
+				# return HttpResponse('ok')
+	else:
+		return redirect('/welcome/')
 def register(request):
 	return render(request,'register.html')
